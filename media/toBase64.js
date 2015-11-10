5b2aa63c1d054dd3bb95467bfa6fd16bb224b187
suhaotian <h4av@live.cn>
2015-11-11 02:12:00 +0800

    change

--- media/toBase64.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-
-var fileName = process.argv[2];
-var fileData = fs.readFileSync('./pfzl.mp3','binary');
-var base64Data = new Buffer(fileData).toString('base64');
-
-fs.writeFileSync(fileName + '.base64', base64Data);
-console.log(fileName);
